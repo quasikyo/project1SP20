1af2e513844015dd706956a5311a09d0eb5cfcce
Michael Rehman <mrr84017@uga.edu>
2021-02-08 01:57:45 -0500

    Implemented findIndex, formatting

--- UnsortedList.cpp
+++ UnsortedList.cpp
@@ -1,6 +1,5 @@
 #include "UnsortedList.h"
 
-
 // No argument constructor
 // Post: Max_Items initialized to 50 and Length have been initialized.
 // The array to hold the list elements has been allocated.
@@ -12,8 +11,8 @@ UnsortedList<ItemType>::UnsortedList() : UnsortedList(50) {} // UnsortedList<Ite
 // Post: Max_Itemms and Length have been initialized.
 // The array to hold the list elements has been allocated.
 template <class ItemType>
-UnsortedList<ItemType>::UnsortedList(int max) : length{0} : Max_Items{max} {
-    info = new ItempType[Max_Items];
+UnsortedList<ItemType>::UnsortedList(int max) : Max_Items{max}, length{0} {
+    info = new ItemType[Max_Items];
 } // UnsortedList<ItemType>::UnsortedList(int)
 
 
@@ -27,6 +26,10 @@ UnsortedList<ItemType>::~UnsortedList() {
 // Post: Length has been reset to the empty state.
 template <class ItemType>
 void UnsortedList<ItemType>::makeEmpty() {
+    // Deallocate memory
+    this->~UnsortedList();
+    // Reset info and length (NOT Max_Items)
+    info = new ItemType[Max_Items];
     length = 0;
 } // UnsortedList<ItemType>::makeEmpty()
 
@@ -47,67 +50,69 @@ bool UnsortedList<ItemType>::isFull() const {
 
 // Function: Adds newItem to the end of the list.
 template <class ItemType>
-void UnsortedList<ItemType>::putItem(ItemType newItem){
-    for(int i = 0; i < length; i++){
-      if(info[i] == newItem){
-	throw DuplicateItem(); //duplicate exception
-      }
-    }
-    
-  if(length + 1 < Max_Items){
-    info[length] = newItem;
-    length++;
-    
-  } else {
-    throw FullList(); //Full list exception
+void UnsortedList<ItemType>::putItem(ItemType newItem) {
+    // Check if there is still space
+    if (this->isFull()) {
+        throw FullList();
+    } // if
 
-  }
-  
-} // UnsortedList<ItemType>::putItem(ItemType newItem)
+    // Check for a duplicate
+    int duplicateItemIndex = findIndex(newItem);
+    if (duplicateItemIndex != -1) {
+        throw DuplicateItem();
+    } // if
+
+    // Add newItem to list
+    info[length] = newItem;
+    ++length;
+} // UnsortedList<ItemType>::putItem(ItemType)
 
 
 // Function: deletes Item from the list.
 template <class ItemType>
-void UnsortedList<ItemType>::deleteItem(ItemType Item){
-    bool inList = false;
-    if(length == 0){
-      throw EmptyList() ; //empty list exception
-    }
-    //going backward to find the last item/possible duplicate
-    //and shifting info by 1 
-    for(int i = 0 ; i < length; i++){
-      if(info[i].equals(Item)){
-	for(int ii = i; ii < length-1; ii++){
-	  info[ii] = info[ii+1];
-	}
-	//length decrement and bool change
-	length--;
-	inList = true;
-	break;
-      }
-    }
+void UnsortedList<ItemType>::deleteItem(ItemType Item) {
+    // No items do delete
+    if (this->isEmpty()) {
+        throw EmptyList();
+    } // if
 
-    if (!inList){
-      throw ItemNotFound(); //item not in list
-    }
-  
-} // UnsortedList<ItemType>::deleteItem()
+    // Search for Item; delete and shift if found, exception otherwise
+    int itemIndex = findIndex(Item);
+    if (itemIndex != -1) {
+        for (int i = itemIndex; i < length - 1; ++i) {
+            info[i] = info[i + 1];
+        } // for
+        --length; // also takes care of edge case itemIndex = length - 1
+    } else {
+        throw ItemNotFound();
+    } // if
+} // UnsortedList<ItemType>::deleteItem(ItemType)
 
 
 // Returns the number of elements in the list.
 template <class ItemType>
 int UnsortedList<ItemType>::getLength() const {
-  return length;
+    return length;
 } // UnsortedList<ItemType>::getLength()
 
 
 // Function: returns the item stored at index in the list
 template <class ItemType>
-ItemType UnsortedList<ItemType>::getAt(int index) const{
-  if(index >= length || index < 0){
-    throw OutofBound();
-  } else{
+ItemType UnsortedList<ItemType>::getAt(int index) const {
+    if (index >= length || index < 0) {
+        throw OutofBound();
+    } // if
     return info[index];
-  }
 } // UnsortedList<ItemType>::getAt()
+
+
+template <class ItemType>
+int UnsortedList<ItemType>::findIndex(ItemType item) const {
+    for (int i = 0; i < length; ++i) {
+        if (info[i] == item) {
+            return i;
+        } // if
+    } // for
+    return -1;
+} // UnsortedList<ItemType>::findIndex(ItemType)
 
